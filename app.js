ca23357ae04788060c93443071f87bb4cd90a02e
stoianstela <stoian.stela12@gmail.com>
2024-06-10 19:55:40 +0300

    second commit

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const bookingRouter = require('./routes/bookingRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
 //Start express app
+//New comment
 const app = express();
 
 app.set('view engine', 'pug');
